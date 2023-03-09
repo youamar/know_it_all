99198fc2d295d1f2f08921c81ae8e3405b9ec0bc
Yahya OUAMAR <54915@etu.he2b.be>
2023-03-09 11:33:43 +0100

    ajout de la navigation et de la vérification de l'adresse

--- KnowItAll/app/src/main/java/com/example/knowitall/LoginFragment.kt
+++ KnowItAll/app/src/main/java/com/example/knowitall/LoginFragment.kt
@@ -1,10 +1,12 @@
 package com.example.knowitall
 
 import android.os.Bundle
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.knowitall.databinding.FragmentLoginBinding
 
@@ -21,6 +23,16 @@ class LoginFragment : Fragment() {
     ): View? {
         val binding = DataBindingUtil.inflate<FragmentLoginBinding>(inflater,
             R.layout.fragment_login, container, false)
+
+        binding.loginButton.setOnClickListener { view: View ->
+            val email = binding.editTextTextEmailAddress.text.toString()
+            if (Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                Toast.makeText(context, "Valid address !", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(context, "Invalid address !", Toast.LENGTH_SHORT).show()
+            }
+        }
+
         return binding.root
     }
 
