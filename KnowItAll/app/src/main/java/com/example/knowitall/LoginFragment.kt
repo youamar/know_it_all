369a04ef58f59bf9f4c2ad9f876c65a83524e477
Yahya OUAMAR <54915@etu.he2b.be>
2023-03-09 09:58:11 +0100

    ajout du fragment de connexion

---
+++ KnowItAll/app/src/main/java/com/example/knowitall/LoginFragment.kt
@@ -0,0 +1,27 @@
+package com.example.knowitall
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.knowitall.databinding.FragmentLoginBinding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LoginFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentLoginBinding>(inflater,
+            R.layout.fragment_login, container, false)
+        return binding.root
+    }
+
+}
